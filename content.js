2bc99120cb29a58a9be1cd9df81c975300598acc
burnedsap <salil458@gmail.com>
2018-07-30 00:00:26 +0530

    Add files via upload
    fixed major flaws in v1.1.5

--- content.js
+++ content.js
@@ -3,18 +3,21 @@ function Skipper() {
   var arr = [];
   var arrS  = [];
 
+  var video = document.getElementsByTagName("video");
   var link = document.links;
-  for (var i = link.length - 1; i > 0; i--) {
-    if (link[i].host == location.host) {
-      arr.push(link[i].href);
-    }
-  }
-  for (var j = 0; j < arr.length; j++) {
-    if(arr[j].indexOf('https://www.youtube.com/watch?v')>= 0) {
-      arrS[j] = arr[j];
-    }
-  }
 
-  var rando =  Math.floor(Math.random()*arrS.length);
-  window.location.href = arrS[rando];
+  if(!video.paused) {
+    for (var i = link.length - 1; i > 0; i--) {
+      if (link[i].host == location.host) {
+        arr.push(link[i].href);
+      }
+    }
+    for (var j = 0; j < arr.length; j++) {
+      if(arr[j].indexOf('https://www.youtube.com/watch?v')>= 0) {
+        arrS[j] = arr[j];
+      }
+    }
+    var rando =  Math.floor(Math.random()*arrS.length);
+    window.location.href = arrS[rando];  
+  }
 }
